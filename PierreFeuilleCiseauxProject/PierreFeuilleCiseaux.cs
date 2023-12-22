7081ca0d98137b7e6923e1812fb12e03de2400e1
Paul MINELLA <p.minella@mondorf.lu>
2023-12-22 11:45:04 +0100

    first commit with VS solution and gitignore

---
+++ PierreFeuilleCiseauxProject/PierreFeuilleCiseaux.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PierreFeuilleCiseauxProject
+{
+    public class PierreFeuilleCiseaux
+    {
+        private static readonly List<string> Symboles = new List<string> { "Pierre", "Feuille", "Ciseaux" };
+
+        public static string JouerTour()
+        {
+            string choixJoueur1 = ChoisirSymboleAleatoire();
+            string choixJoueur2 = ChoisirSymboleAleatoire();
+
+            Console.WriteLine($"Joueur 1 choisit: {choixJoueur1}");
+            Console.WriteLine($"Joueur 2 choisit: {choixJoueur2}");
+
+            string resultat = DeterminerResultat(choixJoueur1, choixJoueur2);
+
+            Console.WriteLine($"Résultat: {resultat}");
+            Console.WriteLine();
+
+            return resultat;
+        }
+
+        public static string ChoisirSymboleAleatoire()
+        {
+            Random random = new Random();
+            int index = random.Next(Symboles.Count);
+            return Symboles[index];
+        }
+
+        public static string DeterminerResultat(string symboleJoueur1, string symboleJoueur2)
+        {
+            if (symboleJoueur1 == symboleJoueur2)
+            {
+                return "Égalité. Rejouez!";
+            }
+            else if ((symboleJoueur1 == "Pierre" && symboleJoueur2 == "Ciseaux") ||
+                     (symboleJoueur1 == "Feuille" && symboleJoueur2 == "Pierre") ||
+                     (symboleJoueur1 == "Ciseaux" && symboleJoueur2 == "Feuille"))
+            {
+                return "Joueur 1 gagne!";
+            }
+            else
+            {
+                return "Joueur 2 gagne!";
+            }
+        }
+    }
+}
---
+++ PierreFeuilleCiseauxUnitTests/PierreFeuilleCiseauxTests.cs
@@ -0,0 +1,48 @@
+﻿using PierreFeuilleCiseauxProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PierreFeuilleCiseauxUnitTests
+{
+    [TestClass]
+    public class PierreFeuilleCiseauxTests
+    {
+        [TestMethod]
+        public void TestJouerTour()
+        {
+            // Arrange & Act
+            string resultat = PierreFeuilleCiseaux.JouerTour();
+
+            // Assert
+            Assert.IsNotNull(resultat);
+        }
+
+        [TestMethod]
+        public void TestChoisirSymboleAleatoire()
+        {
+            // Arrange & Act
+            string symbole = PierreFeuilleCiseaux.ChoisirSymboleAleatoire();
+
+            // Assert
+            CollectionAssert.Contains(new[] { "Pierre", "Feuille", "Ciseaux" }, symbole);
+        }
+
+        [TestMethod]
+        [DataRow("Pierre", "Ciseaux", "Joueur 1 gagne!")]
+        [DataRow("Feuille", "Pierre", "Joueur 1 gagne!")]
+        [DataRow("Ciseaux", "Feuille", "Joueur 1 gagne!")]
+        [DataRow("Pierre", "Feuille", "Joueur 2 gagne!")]
+        [DataRow("Feuille", "Feuille", "Égalité. Rejouez!")]
+        public void TestDeterminerResultat(string symboleJoueur1, string symboleJoueur2, string expectedResult)
+        {
+            // Arrange & Act
+            string resultat = PierreFeuilleCiseaux.DeterminerResultat(symboleJoueur1, symboleJoueur2);
+
+            // Assert
+            Assert.AreEqual(expectedResult, resultat);
+        }
+    }
+}
